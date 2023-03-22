e2e3c95dc6410a5bd1ea8faf99af6cd34518bf11
Tarel-Github <sms_stmax@naver.com>
2023-03-23 01:07:37 +0900

    슬라이딩 윈도우

--- forTest.py
+++ forTest.py
@@ -1,27 +1,9 @@
 # 파이썬 코드 테스트를 위한 파일
-n = int(input())
-A = list(map(int, (input().split())))
+N, n = int(input().split())
 
-A.sort()
-count = 0
-i = 0
-j = n-1 # 끝부분 인덱스가 마지막 번호에서 시작
-tg = 0
+S = int(input().split())
+A, C, G, T = int(input().split())
 
-while tg < n:
-    if A[i] + A[j] > A[tg]:
-        j -= 1
-    elif A[i] + A[j] < A[tg]:
-        i += 1
-    else:   # 정답인 경우
-        tg += 1
-        count += 1
-        i = 0
-        j = n-1
-    
-    if j == 0:  # 없는 경우
-        tg += 1
-        i = 0
-        j = n-1
+
 
 print(count)
