c9aa3f65852f564610c4a34e4c03173245e66a8c
Tarel-Github <sms_stmax@naver.com>
2023-03-26 22:15:02 +0900

    스택과 큐 완료

--- forTest.py
+++ forTest.py
@@ -1,1 +1,13 @@
 # 파이썬 코드 테스트를 위한 파일
+from collections import deque
+N = int(input()) # 카드 수 입력
+myQueue = deque()
+
+for i in range(N):
+    myQueue.append(i+1)
+
+while len(myQueue):
+    myQueue.popleft()
+    myQueue.append(myQueue.popleft())
+
+print(myQueue[0])
