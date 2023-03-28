e5c52b567d3f2494a01b8c83d476ee13545ee2b5
Tarel-Github <sms_stmax@naver.com>
2023-03-28 13:48:58 +0900

    정렬 알고리즘 완료

--- forTest.py
+++ forTest.py
@@ -1,21 +1,13 @@
-# 파이썬 코드 테스트를 위한 파일
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-A = list(map(int, input().split()))
-S = []
-result = 0
-sum = 0
+count =[0] * 10001
 
-for i in range(0, len(A)):
-    S.append(A[i])
-    for j in range(0, len(S)):
-        if S[i] < S[j]:
-            temp = S[j]
-            S[j] = S[i]
-            S[i] = temp
+for i in range(N):
+    count[int(input())] += 1
 
-# 여기까지 정렬 완료
-for i in A:
-    result += i
-    sum += result
-    print(result, sum)
-
+for i in range(10001):
+    if count[i] != 0:
+        for _ in range(count[i]):
+            print(i)
