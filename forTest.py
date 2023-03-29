358d13ff496b9a0a83df439347decad8b0495e91
Tarel-Github <sms_stmax@naver.com>
2023-03-30 00:33:22 +0900

    그리디

--- forTest.py
+++ forTest.py
@@ -1,37 +1,14 @@
-from collections import deque
-N, M, Start = map(int, input().split())
-A=[[]for _ in range(N+1)]
+N, K = map(int, input().split())
+A = [0] *N
 
-for _ in range(M):
-    s, e =map(int, input().split())
-    A[s].append(e)
-    A[e].append(s)
+for i in range(N):
+    A[i] = int(input())
 
-for i in range(N+1):
-    A[i].sort()
+count = 0
 
-def DFS(v):
-    print(v, end= ' ')
-    visited[v] = True
-    for i in A[v]:
-        if not visited[i]:
-            DFS(i)
+for i in range(N - 1, -1, -1):
+    if A[i] <= K:
+        count += int(K/A[i])
+        K = K % A[i]
 
-visited=[False] * (N+1)
-DFS(Start)
-
-def BFS(v):
-    queue = deque()
-    queue.append(v)
-    visited[v] = True
-    while queue:
-        now_Node = queue.popleft()
-        print(now_Node, end = ' ')
-        for i in A[now_Node]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-
-print()
-visited = [False] * (N + 1)
-BFS(Start)
+print(count)
