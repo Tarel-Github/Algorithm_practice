b6f9c3f304ed3e32f5c6270b99eb796bb5c087d4
Tarel-Github <sms_stmax@naver.com>
2023-04-01 01:48:58 +0900

    트리 진입

--- forTest.py
+++ forTest.py
@@ -1,9 +1,42 @@
 import sys
-input = sys.stdin.readline
-N, M = map(int, input().split())
-edges = []
-distance = [sys.maxsize] * (N+1)
+from queue import PriorityQueue
 
+input = sys.stdin.readlines
+N, M = map(int, input().split())
+pq = PriorityQueue()
+parent = [0] * ( N + 1)
+
+for i in range(N+1):
+    parent[i] = i
+
+for i in range(M):
+    s, e, w = map(int, input().split())
+    pq.put((w, s, e))
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] =find(parent[a])
+        return parent[a]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
+useEdge = 0
+result = 0
+
+while useEdge < N - 1:
+    w, s, e = pq.get()
+    if find(s) != find(e):
+        union(s, e)
+        result += w
+        useEdge += 1
+
+print(result)
 
 
 '''
