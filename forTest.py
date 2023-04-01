bc9c793b4453852897ee56f53b4dd35eac28fdbb
Tarel-Github <sms_stmax@naver.com>
2023-04-01 11:13:17 +0900

    기초 탬플릿 완성

--- forTest.py
+++ forTest.py
@@ -1,44 +1,16 @@
 import sys
-from queue import PriorityQueue
+input = sys.stdin.readline
 
-input = sys.stdin.readlines
-N, M = map(int, input().split())
-pq = PriorityQueue()
-parent = [0] * ( N + 1)
+x1, y1 = map(int, input.split())
+x2, y2 = map(int, input.split())
+x3, y3 = map(int, input.split())
 
-for i in range(N+1):
-    parent[i] = i
+result = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)
 
-for i in range(M):
-    s, e, w = map(int, input().split())
-    pq.put((w, s, e))
-
-def find(a):
-    if a == parent[a]:
-        return a
-    else:
-        parent[a] =find(parent[a])
-        return parent[a]
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a != b:
-        parent[b] = a
-
-useEdge = 0
-result = 0
-
-while useEdge < N - 1:
-    w, s, e = pq.get()
-    if find(s) != find(e):
-        union(s, e)
-        result += w
-        useEdge += 1
-
-print(result)
-
-
-'''
-
-'''
+if result > 0:
+    print(1)
+    
+elif result < 0:
+    print(-1)
+else:
+    print(0)
