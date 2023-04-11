7e22caf0be46e785bb8d1578827d6cf981ac8650
Tarel-Github <sms_stmax@naver.com>
2023-04-11 13:17:44 +0900

    투 포인터 완성

--- forTest.py
+++ forTest.py
@@ -1,19 +1,33 @@
-n = int(input())
-count = 1           # 리턴해야할 값, 총 몇 종류가 나오는가?
-start_index = 1
-end_index = 1
-sum = 1             # 더하는 값 저장
+# 최적화를 위한 임포트
+import sys
+input = sys.stdin.readline
 
-while end_index != n:
-    if sum == n:        # 정답인 경우
-        count += 1
-        end_index += 1
-        sum += end_index
-    elif sum > n:       # 두 포인터 사이 값을 전부 더했을 때, n보다 크다면
-        sum -= start_index  # 
-        start_index += 1    # 스타트 값을 올린다.
-    else:               # 두 포인터 사이 값이 n보다 작다면(초반부에 주로 발생)
-        end_index += 1
-        sum += end_index
+# 몇 개의 숫자를 입력받는지를 입력하고, 수열을 입력
+# 정답 변수를 선언하고 수열을 사용하기 좋게 정렬함
+N = int(input())
+A = list(map(int, (input().split())))
+Result = 0
+A.sort()
 
-print(count)
+# 여기서부터 핵심
+# 포인터 i와 j는 시작과 끝부분에서 시작한다.
+# 이 두 포인터를 양쪽에서 조여나가는 식으로 전개한다.
+# 목표값보다 작으면 시작포인터를, 크면 끝포인터를 당기는 방식이다.
+for k in range(N):
+    find = A[k]          # 목표값을 find로 저장
+    i = 0
+    j = N - 1
+    while i < j:
+        if A[i] + A[j] == find:
+            if i != k and j != k:   # 목표값을 찾았을 때, 그 값이 자기자신인 경우는 제외해야 한다.
+                Result += 1
+                break
+            elif i == k:    # 시작포인터나 끝포인터가 목표값이 되는 경우도 제외
+                i += 1
+            elif j == k:
+                j -= 1
+        elif A[i] + A[j] < find:    # 여기서부터는 포인터를 조여오는 구문
+            i += 1
+        else:
+            j -= 1
+print(Result)
