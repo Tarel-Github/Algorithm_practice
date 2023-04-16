700428ca5e85af17eddb426aaca4d044a343ebc8
Tarel-Github <sms_stmax@naver.com>
2023-04-16 18:45:57 +0900

    구간합

--- forTest.py
+++ forTest.py
@@ -1,20 +1,41 @@
-n = int(input())            # 수가 몇 개인지 입력
-ans = [0] * n               # 수의 개수길이 만큼 리스트를 준비, 이건 정답용
-A = list(map(int, input().split())) # 리스트를 완성
-myStack = []                # 스텍을 준비, 이 스텍은 인덱스보다 높은 값을 저장하는 역할을 한다.
+# 임포트
+import sys
+input = sys.stdin.readline
 
-for i in range(n):          
-    print(i, myStack, A, ans)
-    while myStack and A[myStack[-1]] < A[i]:
-        ans[myStack.pop()] = A[i]
-    myStack.append(i)
+# 리스트의 크기와 질의 수를 입력
+n, m = map(int, input().split())
 
-while myStack:
-    ans[myStack.pop()] = -1
+'''
+아래 A는 만약 n이 5면 다음과 같이 나온다. 보시다시피 한 칸 더 나온다.
+A = [[0, 0, 0, 0, 0, 0]]
 
-result = ""
+D 는 아래와 같이 나온다. 마찬가지로, 행렬 각각 한 칸 더 나온다.
+[[0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0]]
 
+'''
+A = [[0] * ( n + 1 )]
+D = [[0] * ( n + 1 ) for _ in range(n+1)]           # [[0] * ( n + 1 )] * (n + 1) 이렇게 적어도 됌
+
+# 각 배열들의 요소를 입력받아서 2차원 배열을 완성하기 위한 부분
 for i in range(n):
-    result += str(ans[i]) + " "
+    A_row = [0] + [int(x) for x in input().split()]
+    A.append(A_row)
 
-print(result)
+print(A)
+print(D)
+# 이곳이 핵심, 2차원 합배열을 완성하는 반복문이다.
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        D[i][j] = D[i][j - 1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
+
+# 어디서부터 어디까지 더할지를 입력받는다.
+for i in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+ 
+    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1 - 1][y1 - 1]
+    print(result)
